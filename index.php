6d88d8cc74749816510d35b8c7f3897b86673efb
przemekbarczyk <przemek.barczyk@gmail.com>
2020-11-12 12:50:20 +0100

    Added routing and template renderer

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo "Hi world!";
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Routing::get('index', 'DefaultController');
+Routing::get('register', 'DefaultController');
+Routing::get('all_projects', 'DefaultController');
+Routing::get('my_projects', 'DefaultController');
+Routing::get('add_project', 'DefaultController');
+Routing::get('project_details', 'DefaultController');
+
+Routing::run($path);
