a2ac2f380ce8dde91dd0b6c90cf4b1437f90ed16
przemekbarczyk <przemek.barczyk@gmail.com>
2020-12-03 13:53:04 +0100

    Connect to database, install pdo drivers, create user repository

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,21 +2,27 @@
 
 require_once 'AppController.php';
 require_once __DIR__.'/../models/User.php';
+require_once __DIR__.'/../repository/UserRepository.php';
 
 class SecurityController extends AppController {
 
     public function login() {
+        $userRepository = new UserRepository();
 
-        $user = new user('jsnow@pk.edu.pl', 'admin', 'John', 'Snow');
-
-        if (!$this->isPost()) { //
+        if (!$this->isPost()) {
             return $this->render('login');
-        } else if (isset($_POST['left-button'])) { // naciśnięto lewy przycisk
+        } else if (isset($_POST['left-button'])) { // pressed register button
             return $this->render('register');
         }
 
         $email = $_POST["email"];
         $password = $_POST["password"];
+
+        $user = $userRepository->getUser($email);
+
+        if (!$user) {
+            return $this->render('login', ['messages' => ['Użytkownik nie istnieje!']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['Użytkownik o podanym adresie email nie istnieje!']]);
